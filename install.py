5dbea978d2d7c5f6e0ff9edf27eb40ba67d303ce
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-12-10 12:38:12 +0000

    Update install.py

--- install.py
+++ install.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 import os
+import shutil
 
+# Obtain and include the installLib library.
 os.system("git clone https://github.com/dhicks6345789/install-lib.git")
+shutil.copyfile("install-lib/installLib.py", ".")
 
 print("Hello, world!")
