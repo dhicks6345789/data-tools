7d0aa5c1fa29facc9d8e4a27d6f5b0525bec4080
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-12-11 13:13:46 +0000

    Update install.py

--- install.py
+++ install.py
@@ -80,6 +80,7 @@ config = json.loads(installLib.readFile("config/config.json"))
 for requiredConfigParameter in requiredConfigParameters:
 	if not requiredConfigParameter in config.keys():
 		print("Error - required value " + requiredConfigParameter + " not set in config.json.")
+		sys.exit(1)
 
 
 
@@ -88,3 +89,4 @@ for requiredConfigParameter in requiredConfigParameters:
 
 
 # Code goes here - more stuff.
+print config["iSAMSAPIKey"]
