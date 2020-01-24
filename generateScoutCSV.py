0887fbda99b7935975336d61e1a9db06d3b1b354
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-01-24 15:24:39 +0000

    Create generateScoutCSV.py

---
+++ generateScoutCSV.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import json
+import installLib
+
+requiredConfigParameters = ["inputFile", "outputFolder"]
+
+# Load the configuration file.
+config = json.loads(installLib.readFile("config/config.json"))
+for requiredConfigParameter in requiredConfigParameters:
+	if not requiredConfigParameter in config.keys():
+		print("Error - required value " + requiredConfigParameter + " not set in config.json.")
+		sys.exit(1)
