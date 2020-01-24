fbc598a9665c8454702832e0aaf4f96b293cdaa6
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-01-24 16:29:47 +0000

    Update generateScoutCSV.py

--- generateScoutCSV.py
+++ generateScoutCSV.py
@@ -16,9 +16,12 @@ for requiredConfigParameter in requiredConfigParameters:
 print(config["pupilsCSVInputFile"])
 print(config["outputFolder"] + "\\Scout\\allUsersForBorrowerImport.csv")
 
-print("P1949,kskirkoli15,Student,Library,OPAC Only,Pupils,Olivia,Kirkup,S5C,28/01/2009")
+# Orig: P1949,kskirkoli15,Student,Library,OPAC Only,Pupils,Olivia,Kirkup,S5C,28/01/2009
+
+#'person_id', 'person_number', 'first_name', 'middle_name', 'last_name', 'grade_level', 'email_address', 'sis_username', 'password_policy', 'location_id'
+#'104', '104', 'Timothy', '', 'Nasser', 'S4L', 't.nasser16@knightsbridgeschool.com', '104', '4', 'KnightsbridgeSchool'
 
 pupils = open(config["pupilsCSVInputFile"])
 pupilsReader = csv.reader(pupils, delimiter=',')
 for pupilRow in pupilsReader:
-	print(pupilRow)
+	print(pupilRow["person_id"] + "," + pupilRow[""] + "Student,Library,OPAC Only,Pupils," + pupilRow["first_name"] + "," + pupilRow["last_name"] + "," + pupilRow["grade_level"])
