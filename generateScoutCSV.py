816935260883e5b546a33b46d15398cb6c2a4f5c
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-02-03 10:17:36 +0000

    Update generateScoutCSV.py

--- generateScoutCSV.py
+++ generateScoutCSV.py
@@ -19,7 +19,7 @@ print(config["outputFolder"] + "\\Scout\\allUsersForBorrowerImport.csv")
 # Orig: P1949,kskirkoli15,Student,Library,OPAC Only,Pupils,Olivia,Kirkup,S5C,28/01/2009
 
 #'person_id', 'person_number', 'first_name', 'middle_name', 'last_name', 'grade_level', 'email_address', 'sis_username', 'password_policy', 'location_id'
-#'104', '104', 'Timothy', '', 'Nasser', 'S4L', 't.nasser16@knightsbridgeschool.com', '104', '4', 'KnightsbridgeSchool'
+#'104', '104', 'Bob', '', 'Smit', 'S4Q', 'b.smith16@knightsbridgeschool.com', '104', '4', 'KnightsbridgeSchool'
 
 pupils = open(config["pupilsCSVInputFile"])
 pupilsReader = csv.reader(pupils, delimiter=',')
