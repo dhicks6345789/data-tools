7f707c2576e89deb3c11328416c0772a59a3ad91
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-02-03 13:18:56 +0000

    Update generateBaseCSV.py

--- generateBaseCSV.py
+++ generateBaseCSV.py
@@ -18,8 +18,9 @@ for requiredConfigParameter in requiredConfigParameters:
 iSAMSXML = xml.etree.ElementTree.fromstring(installLib.readFile("iSAMSData.xml"))
 for currentStaffMember in iSAMSXML.findall("./HRManager/CurrentStaff/StaffMember"):
 	if currentStaffMember.find("Surname").text == "Hicks":
-		print(currentStaffMember)
-	
+		for currentStaffMemberAttribute in currentStaffMember:
+			print(currentStaffMemberAttribute.tag, currentStaffMemberAttribute.attrib)
+
 # Output format:
 # PupilID,GivenName,FamilyName,DateOfBirth,Gender,Username,YearGroup,Form,Tutor
 pupils = pandas.DataFrame({"PupilID":[],"GivenName":[]})
