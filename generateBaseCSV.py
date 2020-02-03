433994b0cbab86626f305e6983596559649ffcec
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-02-03 12:39:24 +0000

    Create generateBaseCSV.py

---
+++ generateBaseCSV.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys
+import json
+import pandas
+import installLib
+
+requiredConfigParameters = ["outputFolder"]
+
+# Load the configuration file.
+config = json.loads(installLib.readFile("config/config.json"))
+for requiredConfigParameter in requiredConfigParameters:
+	if not requiredConfigParameter in config.keys():
+		print("Error - required value " + requiredConfigParameter + " not set in config.json.")
+		sys.exit(1)
+
+print("Hello, world!")
