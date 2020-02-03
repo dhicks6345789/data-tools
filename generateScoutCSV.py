e14b5d034ee57ad7fcb3c53bfd8e82b07020c90d
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-02-03 17:14:16 +0000

    Update generateScoutCSV.py

--- generateScoutCSV.py
+++ generateScoutCSV.py
@@ -30,6 +30,7 @@ for pupilIndex, pupil in pupils.iterrows():
 
 staff = pandas.read_csv(config["dataFolder"] + os.sep + "staff.csv", header=0)
 for staffIndex, staff in staff.iterrows():
+	print(str(staff["GUID"]) + "," + staff["Username"] + ",Staff,Library,Staff,Staff," + staff["GivenName"] + "," + staff["FamilyName"]+ ",," + str(staff["DateOfBirth"]))
 	outputString = outputString + str(staff["GUID"]) + "," + staff["Username"] + ",Staff,Library,Staff,Staff," + staff["GivenName"] + "," + staff["FamilyName"]+ ",," + str(staff["DateOfBirth"]) + "\n"
 	
 installLib.writeFile(config["dataFolder"] + os.sep + "Scout" + os.sep + "allUsersForBorrowerImport.csv", outputString)
