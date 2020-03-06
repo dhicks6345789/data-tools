45b702047af920d35b83794a2989b0fe639fc2b2
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-06 14:13:53 +0000

    Update getData.py

--- getData.py
+++ getData.py
@@ -15,4 +15,4 @@ for requiredConfigParameter in requiredConfigParameters:
     
 # Go and get the data from the iSAMS API.
 response = urllib.request.urlopen("https://" + config["iSAMSAPIDomain"] + "/api/batch/1.0/xml.ashx?apiKey=" + config["iSAMSAPIKey"], context=ssl._create_unverified_context())
-installLib.writeFile("iSAMSData.xml", response.read())
+installLib.writeFile("iSAMSData.xml", str(response.read()))
