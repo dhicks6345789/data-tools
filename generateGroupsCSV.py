5530044e843af98215097cd2ade915464bcbef42
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-10 15:39:28 +0000

    Update generateGroupsCSV.py

--- generateGroupsCSV.py
+++ generateGroupsCSV.py
@@ -8,7 +8,7 @@ import pandas
 import installLib
 
 requiredConfigParameters = ["dataFolder"]
-groupDetails = {"KS-SEC-PUPILS-S8":{"Form":"8"}}
+groupDetails = {"KS-SEC-PUPILS-S8":{"Email":"s8pupils@knightsbridgeschool.com","Form":"8"}}
 
 # Load the configuration file.
 config = json.loads(installLib.readFile("config/config.json"))
@@ -29,11 +29,11 @@ pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
 os.makedirs(config["dataFolder"] + os.sep + "Groups", exist_ok=True)
 for group in groupDetails.keys():
   print(group)
-  outputString = ""
+  outputString = "Group Email [Required],Member Email [Required],Member Type,Member Role\n"
   for pupilIndex, pupil in pupils.iterrows():
     print(groupDetails[group]["Form"] + " == " + pupil["Form"])
     if not re.match(".*" + groupDetails[group]["Form"] + ".*", pupil["Form"]) == None:
-      outputString = outputString + pupil["OldUsername"] + "\n"
+      outputString = outputString + groupDetails[group]["Email"] + "," + pupil["OldUsername"] + "@knightsbridgeschool.com,USER,MEMBER\n"
   installLib.writeFile(config["dataFolder"] + os.sep + "Groups" + os.sep + group + ".csv", outputString)
 	
 #staff = pandas.read_csv(config["dataFolder"] + os.sep + "staff.csv", header=0)
