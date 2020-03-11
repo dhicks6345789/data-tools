5d350904603b53ef77b9b3dfdf12c20bb8054504
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-11 12:28:45 +0000

    Update generateGroupsCSV.py

--- generateGroupsCSV.py
+++ generateGroupsCSV.py
@@ -8,6 +8,8 @@ import pandas
 import installLib
 
 requiredConfigParameters = ["dataFolder"]
+
+# Set up yeargroups.
 groupDetails = {
 	"KS-SEC-PUPILS-NURSERY":{"Email":"nurserypupils@knightsbridgeschool.com","Form":"Lions"},
 	"KS-SEC-PUPILS-RECEPTION":{"Email":"receptionpupils@knightsbridgeschool.com","Form":"Rec"},
@@ -40,7 +42,7 @@ pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
 # Get a list of forms (excluding Nursery and Year 7 and 8, as they only have the one class).
 forms={}
 for pupilIndex, pupil in pupils.iterrows():
-	for yearGroup in ["1","2","3","4","5","6"]:
+	for yearGroup in ["1","2","3","4","5","6","Rec"]:
 		if yearGroup in pupil["Form"]:
 			forms[pupil["Form"]] = 1
 
