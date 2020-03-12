cc1397201c8f8c478786e9947a47a9df66531bbf
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-12 12:50:15 +0000

    Update generateClasses.py

--- generateClasses.py
+++ generateClasses.py
@@ -21,17 +21,14 @@ for requiredConfigParameter in requiredConfigParameters:
   if not requiredConfigParameter in config.keys():
     print("Error - required value " + requiredConfigParameter + " not set in config.json.")
     sys.exit(1)
-
-# Read the existing basic pupils data.
-pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
-
-# Get a list of years and forms.
-forms={"NUSERY":1,"RECEPTION":1,"J1":1,"J2":1,"J3":1,"S4":1,"S5":1,"S6":1,"S7":1,"S8":1}
-for pupilIndex, pupil in pupils.iterrows():
-  forms[pupil["Form"]] = 1  
-forms = forms.keys()
-
-print(forms)
+    
+# Get a list of groups.
+groups = []
+for item in os.listdir(config["dataFolder"] + os.sep + "Groups"):
+  if item.startswith("KS-SEC-PUPILS-"):
+    groups.append(item[14:])
+    
+print(groups)
 
 # Read the Courses list. Courses CSV headings:
 # id,name,courseState,descriptionHeading,description,section,room,enrollmentCode,guardiansEnabled,alternateLink,ownerId,creationTime,updateTime,calendarId,courseGroupEmail,teacherGroupEmail,teacherFolder.id,teacherFolder.title,teacherFolder.alternateLink,courseMaterialSets,courseMaterialSets.0.materials.0.driveFile.alternateLink,courseMaterialSets.0.materials.0.driveFile.id,courseMaterialSets.0.materials.0.driveFile.title,courseMaterialSets.0.title
@@ -41,11 +38,13 @@ unHandledCourses = []
 for courseIndex, course in courses.iterrows():
   if course["courseState"] == "ACTIVE":
     formFound = False
-    for form in forms:
-      if form in normaliseYearNames(course["name"]):
-        formFound = True
-        print("Assign " + form + " to " + course["name"])
-    if formFound == False:
+    for group in group:
+      if group in normaliseYearNames(course["name"]):
+        groupFound = True
+        print("Assign " + group + " to " + course["name"])
+        # Read the existing basic pupils data.
+        #pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
+    if groupFound == False:
       unHandledCourses.append(course["name"])
 
 print("Unhandled courses:")
