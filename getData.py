584fc67cc879779cb002715095bf0e7accd3db4a
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-12 12:14:03 +0000

    Update getData.py

--- getData.py
+++ getData.py
@@ -17,3 +17,6 @@ for requiredConfigParameter in requiredConfigParameters:
 print("Getting data from iSAMS.")
 response = urllib.request.urlopen("https://" + config["iSAMSAPIDomain"] + "/api/batch/1.0/xml.ashx?apiKey=" + config["iSAMSAPIKey"], context=ssl._create_unverified_context())
 installLib.writeFile("iSAMSData.xml", str(response.read())[2:-1])
+
+# Get a list of all courses, output in CSV format directly from GAM.
+os.system("gam print courses > " + config["dataFolder"] + os.sep + "courses.csv")
