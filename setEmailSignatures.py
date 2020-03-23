d26e1477ba0bedb6457635329e9d6a68ccbe452a
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-23 15:27:09 +0000

    Create setEmailSignatures.py

---
+++ setEmailSignatures.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import os
+import re
+import sys
+import csv
+import json
+import pandas
+import installLib
+
+requiredConfigParameters = ["dataFolder"]
+
+# Load the configuration file.
+config = json.loads(installLib.readFile("config/config.json"))
+for requiredConfigParameter in requiredConfigParameters:
+  if not requiredConfigParameter in config.keys():
+    print("Error - required value " + requiredConfigParameter + " not set in config.json.")
+    sys.exit(1)
+
+# Input data headings:
+# Staff: GUID,UserCode,Title,GivenName,FamilyName,DateOfBirth,Username,Identifier,Form,JobTitle
+
+# Read the existing basic staff details.
+staff = pandas.read_csv(config["dataFolder"] + os.sep + "staff.csv", header=0)
+
+outputString = ""
+for staffIndex, staff in staff.iterrows():
+  print(staff["Username"])
