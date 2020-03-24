789233a5454f12ba13963b4d1051c75d2066ee9f
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-24 12:56:53 +0000

    Update setEmailSignatures.py

--- setEmailSignatures.py
+++ setEmailSignatures.py
@@ -49,7 +49,6 @@ for staffIndex, staffMember in staff.iterrows():
       print("Username mismatch: " + staffUsername)
     else:
       staffMember["JobTitle"] = staffJobTitle
-print(staff.to_csv(index=False))
-#installLib.writeFile(config["dataFolder"] + os.sep + "staff.csv", staff.to_csv(index=False))
+installLib.writeFile(config["dataFolder"] + os.sep + "staff.csv", staff.to_csv(index=False))
 #  if staff["Username"] == "d.hicks":
 #    print("gam user " + staff["Username"] + "@knightsbridgeschool.com signature file config/emailSignature.html replyto " + staff["Username"] + "@knightsbridgeschool.com replace name \"" + staff["GivenName"] + " " + staff["FamilyName"] + "\" replace role \"" + staff["Role"] + "\"")
