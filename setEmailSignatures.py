127c2adcf30c3559a275e541027434618dddf2b4
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-24 12:08:16 +0000

    Update setEmailSignatures.py

--- setEmailSignatures.py
+++ setEmailSignatures.py
@@ -34,5 +34,9 @@ for staffIndex, staff in staff.iterrows():
       if not matchResult == None:
         staffName = matchResult[1]
         staffJobTitle = matchResult[2]
+      matchResult = re.match(".*blank..(.*)./a..*", sigLine.strip())
+      if not matchResult == None:
+        staffEmail = matchResult[1]
+        print(staffEmail)
 #  if staff["Username"] == "d.hicks":
 #    print("gam user " + staff["Username"] + "@knightsbridgeschool.com signature file config/emailSignature.html replyto " + staff["Username"] + "@knightsbridgeschool.com replace name \"" + staff["GivenName"] + " " + staff["FamilyName"] + "\" replace role \"" + staff["Role"] + "\"")
