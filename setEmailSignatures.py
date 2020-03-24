567fe019e714f4b4124f62744ce125c6ac0c9703
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-24 11:43:58 +0000

    Update setEmailSignatures.py

--- setEmailSignatures.py
+++ setEmailSignatures.py
@@ -30,7 +30,7 @@ for staffIndex, staff in staff.iterrows():
   # gam user [signature <signature text>] [file <signature file>] [replyto <EmailAddress>] (replace <Tag> <String>)*
   if staff["Username"] == "s.colaco":
     for sigLine in installLib.runCommand("gam user " + staff["Username"] + " show signature"):
-      matchResult = re.match(".(.*)\.span. | (.*)./div.", sigLine.strip())
+      matchResult = re.match("(.*).span. | (.*)./div.", sigLine.strip())
       if not matchResult == None:
         print("Found!")
         print(matchResult[1])
