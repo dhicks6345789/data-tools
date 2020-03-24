9343d8d37c1c44c5e85bffe6c9cb108b24d2a853
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-24 10:50:59 +0000

    Update setEmailSignatures.py

--- setEmailSignatures.py
+++ setEmailSignatures.py
@@ -17,13 +17,19 @@ for requiredConfigParameter in requiredConfigParameters:
     sys.exit(1)
 
 # Input data headings:
-# Staff: GUID,UserCode,Title,GivenName,FamilyName,DateOfBirth,Username,Identifier,Form,JobTitle
+# Staff: GUID,UserCode,Title,GivenName,FamilyName,DateOfBirth,Username,Identifier,Form,Role,JobTitle
 
 # Read the existing basic staff details.
 staff = pandas.read_csv(config["dataFolder"] + os.sep + "staff.csv", header=0)
 
+# Staff job titles: not recorded by iSAMS, but instead set manually in GSuite for each signature. Therefore, for each user, first
+# retrive the existing signature and extract the "job title" value, updating the "staff" records reqd from CSV above. Use the job
+# Role if no other value is present, then write out the updated "staff.csv" again with the added values.
 outputString = ""
 for staffIndex, staff in staff.iterrows():
   # gam user [signature <signature text>] [file <signature file>] [replyto <EmailAddress>] (replace <Tag> <String>)*
-  if staff["Username"] == "d.hicks":
-    print("gam user " + staff["Username"] + "@knightsbridgeschool.com signature file config/emailSignature.html replyto " + staff["Username"] + "@knightsbridgeschool.com replace name \"" + staff["GivenName"] + " " + staff["FamilyName"] + "\" replace role \"" + staff["Role"] + "\"")
+  if staff["Username"] == "s.harrison":
+    signature = installLib.runcommand("gam user " + staff["Username"] + " show signature")
+    print(signature)
+#  if staff["Username"] == "d.hicks":
+#    print("gam user " + staff["Username"] + "@knightsbridgeschool.com signature file config/emailSignature.html replyto " + staff["Username"] + "@knightsbridgeschool.com replace name \"" + staff["GivenName"] + " " + staff["FamilyName"] + "\" replace role \"" + staff["Role"] + "\"")
