c56ae07fce2bd7ad389605c78f237dec39671c5b
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-24 11:17:24 +0000

    Update setEmailSignatures.py

--- setEmailSignatures.py
+++ setEmailSignatures.py
@@ -30,6 +30,8 @@ for staffIndex, staff in staff.iterrows():
   # gam user [signature <signature text>] [file <signature file>] [replyto <EmailAddress>] (replace <Tag> <String>)*
   if staff["Username"] == "s.colaco":
     for sigLine in installLib.runCommand("gam user " + staff["Username"] + " show signature"):
-      print(sigLine.strip())
+      matchResult = re.match("font-weight:bold\">(.*)</span> | (.*)</div>", sigLine.strip())
+      if not matchResult == None:
+        print(matchResult[0])
 #  if staff["Username"] == "d.hicks":
 #    print("gam user " + staff["Username"] + "@knightsbridgeschool.com signature file config/emailSignature.html replyto " + staff["Username"] + "@knightsbridgeschool.com replace name \"" + staff["GivenName"] + " " + staff["FamilyName"] + "\" replace role \"" + staff["Role"] + "\"")
