ccfe9d6ce50d84dc773f89587d1146604bbad63e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-26 15:54:42 +0000

    Update generateGroupsCSV.py

--- generateGroupsCSV.py
+++ generateGroupsCSV.py
@@ -78,6 +78,6 @@ outputString = ""
 for staffIndex, staff in staff.iterrows():
 	if not str(staff["Username"]) == "nan":
 		outputString = outputString + str(staff["Username"]).lower() + "@knightsbridgeschool.com\n"
-installLib.writeFile(config["dataFolder"] + os.sep + "Groups" + os.sep + "staff.csv", outputString)
+installLib.writeFile(config["dataFolder"] + os.sep + "Groups" + os.sep + "Staff.csv", outputString)
 print("Sync group Staff from CSV to GSuite.")
 print("gam update group staff@knightsbridgeschool.com sync member file \"" + config["dataFolder"] + os.sep + "Groups" + os.sep + "KS-SEC-STAFF.csv\" 2>&1")					
