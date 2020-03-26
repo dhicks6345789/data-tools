e699ba448800b7470bd2487ef096b618d8de75e8
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-26 16:10:59 +0000

    Create syncGoogleGroupsToCSV.py

---
+++ syncGoogleGroupsToCSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import os
+import re
+import sys
+import csv
+import json
+import pandas
+import installLib
+
+requiredConfigParameters = ["dataFolder"]
+
+# Load the configuration file.
+config = json.loads(installLib.readFile("config/config.json"))
+for requiredConfigParameter in requiredConfigParameters:
+	if not requiredConfigParameter in config.keys():
+		print("Error - required value " + requiredConfigParameter + " not set in config.json.")
+		sys.exit(1)
+
+# Create a CSV file for each group (i.e. Year Group or Form).
+#os.makedirs(config["dataFolder"] + os.sep + "Groups", exist_ok=True)
+print("gam update group " + groupDetails[group]["email"].lower() + " sync member file \"" + config["dataFolder"] + os.sep + "Groups" + os.sep + group + ".csv\" 2>&1")
