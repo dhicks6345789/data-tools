1a91cba0609ec6573e8274c26162c7b168fc4391
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-26 15:54:00 +0000

    Update generateGroupsCSV.py

--- generateGroupsCSV.py
+++ generateGroupsCSV.py
@@ -68,14 +68,16 @@ for group in groupDetails.keys():
 	for member in groupDetails[group]["Members"]:
 		outputString = outputString + member + "\n"
 	installLib.writeFile(config["dataFolder"] + os.sep + "Groups" + os.sep + group + ".csv", outputString)
+	print("Sync group " + group + " from CSV to GSuite.")
 	print("gam update group " + groupDetails[group]["email"].lower() + " sync member file \"" + config["dataFolder"] + os.sep + "Groups" + os.sep + group + ".csv\" 2>&1")
 	
 ## Read the existing basic staff details.
-#staff = pandas.read_csv(config["dataFolder"] + os.sep + "staff.csv", header=0)
+staff = pandas.read_csv(config["dataFolder"] + os.sep + "staff.csv", header=0)
 
-#outputString = ""
-#for staffIndex, staff in staff.iterrows():
-#  if not str(staff["Username"]) == "nan":
-#    outputString = outputString + str(staff["Username"]).lower() + "@knightsbridgeschool.com\n"
-#installLib.writeFile(config["dataFolder"] + os.sep + "Groups" + os.sep + "KS-SEC-STAFF.csv", outputString)
-#os.system("gam update group staff@knightsbridgeschool.com sync member file \"" + config["dataFolder"] + os.sep + "Groups" + os.sep + "KS-SEC-STAFF.csv\" 2>&1")					
+outputString = ""
+for staffIndex, staff in staff.iterrows():
+	if not str(staff["Username"]) == "nan":
+		outputString = outputString + str(staff["Username"]).lower() + "@knightsbridgeschool.com\n"
+installLib.writeFile(config["dataFolder"] + os.sep + "Groups" + os.sep + "staff.csv", outputString)
+print("Sync group Staff from CSV to GSuite.")
+print("gam update group staff@knightsbridgeschool.com sync member file \"" + config["dataFolder"] + os.sep + "Groups" + os.sep + "KS-SEC-STAFF.csv\" 2>&1")					
