4a09bb259119b333491dd5b18f4a9915ab7ebe2d
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-27 14:52:34 +0000

    Create generateParentsCSV.py

---
+++ generateParentsCSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import os
+import re
+import sys
+import csv
+import json
+import pandas
+import installLib
+
+requiredConfigParameters = ["dataFolder"]
+
+# Load the configuration file.
+config = json.loads(installLib.readFile("config/config.json"))
+for requiredConfigParameter in requiredConfigParameters:
+	if not requiredConfigParameter in config.keys():
+		print("Error - required value " + requiredConfigParameter + " not set in config.json.")
+		sys.exit(1)
+
+# Input data headings:
+# Pupils: GUID,UserCode,GivenName,FamilyName,DateOfBirth,Gender,Username,OldUsername,YearGroup,Form,Tutor
+
+# Read the existing basic pupils data.
+pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
+
+#for pupilIndex, pupil in pupils.iterrows():
+#	form = pupil["Form"]
+#	if not form in yeargroups:
+#		forms[form] = 1
