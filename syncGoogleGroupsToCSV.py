1750614a4d2ec31db40104642b48905ad90238f7
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-27 14:27:59 +0000

    Update syncGoogleGroupsToCSV.py

--- syncGoogleGroupsToCSV.py
+++ syncGoogleGroupsToCSV.py
@@ -23,8 +23,7 @@ groups = pandas.read_csv(config["dataFolder"] + os.sep + "groups.csv", header=0)
 os.makedirs(config["dataFolder"] + os.sep + "Groups", exist_ok=True)
 print("erase " + config["dataFolder"] + os.sep + "Groups" + os.sep + "*.*")
 for groupIndex, group in groups.iterrows():
-	print(group["name"] + ":")
 	outputString = ""
 	for member in str(group["Members"]).split():
 		outputString = outputString + member + ",\n"
-	print(outputString)
+	installLib.writeFile(config["dataFolder"] + os.sep + "Groups" + os.sep + group["name"] + ".csv", outputString)
