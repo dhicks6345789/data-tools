ba27423c278036fe5544e7fddbb460bb6dce2509
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-28 18:10:43 +0000

    Update setPupilPasswords.py

--- setPupilPasswords.py
+++ setPupilPasswords.py
@@ -15,7 +15,14 @@ for requiredConfigParameter in requiredConfigParameters:
 	if not requiredConfigParameter in config.keys():
 		print("Error - required value " + requiredConfigParameter + " not set in config.json.")
 		sys.exit(1)
-		
+
+# Read the contens of a file.
+def readFile(theFilename):
+	inHandle = open(theFilename)
+	inData = inHandle.read()
+	inHandle.close()
+	return inData
+
 # Read the existing basic pupils data.
 pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
 
