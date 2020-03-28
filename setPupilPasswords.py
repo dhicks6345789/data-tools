3ddd97ae56313ecedfbccc336726b891824ef8cd
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-28 18:05:18 +0000

    Update setPupilPasswords.py

--- setPupilPasswords.py
+++ setPupilPasswords.py
@@ -19,6 +19,17 @@ for requiredConfigParameter in requiredConfigParameters:
 # Read the existing basic pupils data.
 pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
 
+# Read the Forms and Yeargroups into one list.
+classGroups = []
+for yeargroup in readFile(config["dataFolder"] + os.sep + "yeargroups.csv").split("\n"):
+	if not yeargroup == "" and not yeargroup in classGroups:
+		classGroups.append(yeargroup)
+for form in readFile(config["dataFolder"] + os.sep + "forms.csv").split("\n"):
+	if not form == "" and not form in classGroups:
+		classGroups.append(form)
+
 os.makedirs(config["dataFolder"] + os.sep + "DefaultPupilPasswords", exist_ok=True)
 for pupilIndex, pupil in pupils.iterrows():
-	print(pupil["Username"])
+	print(pupil["OldUsername"])
+
+print(classGroups)
