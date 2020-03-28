8352f31474369f6b0cb3ce72b743cec112dec96a
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-28 19:12:40 +0000

    Update setPupilPasswords.py

--- setPupilPasswords.py
+++ setPupilPasswords.py
@@ -56,7 +56,6 @@ for pupilIndex, pupil in pupils.iterrows():
 		if pupil["ID"] == passwordEntry["ID"]:
 			pupilFound = True
 	if not pupilFound:
-		print(pupil["OldUsername"])
-		defaultPasswords.append([pupil["ID"],generatePassword()])
+		defaultPasswords = defaultPasswords.append([pupil["ID"],generatePassword()])
 
 print(defaultPasswords)
