873fad2502367364a66847349d1960b43a975870
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-28 19:05:06 +0000

    Update setPupilPasswords.py

--- setPupilPasswords.py
+++ setPupilPasswords.py
@@ -16,6 +16,13 @@ for requiredConfigParameter in requiredConfigParameters:
 		print("Error - required value " + requiredConfigParameter + " not set in config.json.")
 		sys.exit(1)
 
+# Words used to generate passwords.
+adjectives = ["Large","Small","Long","Short","Narrow","Deep","Flat","Whole","Low","High","Near","Far","Gone","Moving","Fast","Quick","Early","Late","Bright","Dark","Cloudy","Hot","Warm","Cool","Cold","Windy","Noisy","Loud","Quiet","Dry","Wet","Clear","Hard","Soft","Heavy","Light","Strong","Weak","Clean","Tidy","Dirty","Empty","Full","Thirsty","Hungry","Old","Fresh","Dead","Healthy","Sweet","Sour","Bitter","Salty","Good","Bad","Great","Useful","Cheap","Free","Strong","Weak","Able","Rich","Brave","Fine","Sad","Proud","Happy","Clever","Famous","Funny","Kind","Polite","Fair","Busy","Lazy","Lucky","Safe"]
+nouns = ["Albatross","Antelope","Ant","Ape","Baboon","Badger","Bat","Bear","Beaver","Bee","Bird","Bittern","Boar","Buffalo","Caribou","Cat","Cow","Chicken","Coot","Crane","Crocodile","Crow","Deer","Dog","Dolphin","Dove","Duck","Eagle","Eel","Elephant","Elk","Falcon","Ferret","Fish","Fox","Frog","Goose","Giraffe","Gnat","Goat","Gorilla","Grouse","Hawk","Heron","Hornet","Horse","Hyena","Ibex","Jay","Jellyfish","Kangaroo","Kitten","Lark","Lion","Mackerel","Magpie","Mouse","Mole","Monkey","Moose","Owl","Oyster","Parrot","Penguin","Pheasant","Pig","Quail","Rabbit","Raccoon","Rat","Raven","Rook","Seagull","Seal","Sheep","Snake","Sparrow","Squirrel","Stork","Swan","Tiger","Toad","Trout","Turkey","Turtle","Vulture","Weasel","Whale","Wolf","Wombat","Wren","Zebra"]
+# Returns a string containing a new password.
+def generatePassword():
+    return adjectives[random.randint(0, len(adjectives)-1)] + nouns[random.randint(0, len(nouns)-1)] + str(random.randint(10,99))
+
 # Read the contens of a file.
 def readFile(theFilename):
 	inHandle = open(theFilename)
@@ -48,6 +55,6 @@ for pupilIndex, pupil in pupils.iterrows():
 		if pupil["ID"] == passwordEntry["ID"]:
 			pupilFound = True
 	if not pupilFound:
-		print(pupil["OldUsername"])
+		defaultPasswords.append(pupil["ID"],generatePassword())
 
 print(defaultPasswords)
