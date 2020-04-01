6cee6d541c5e7c039f3dc7736c89865b4d0d06da
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-04-01 13:05:40 +0100

    Update generateGroupsCSVsFromiSAMS.py

--- generateGroupsCSVsFromiSAMS.py
+++ generateGroupsCSVsFromiSAMS.py
@@ -71,14 +71,14 @@ for group in groupDetails.keys():
 		outputString = outputString + member + "\n"
 		allPupils = allPupils + member + "\n"
 	installLib.writeFile(config["dataFolder"] + os.sep + "Groups" + os.sep + group + ".csv", outputString)
-	print("Sync group " + group + " from CSV to GSuite.")
-	os.system("gam update group " + groupDetails[group]["email"].lower() + " name \"" + group + "\" 2>&1")
-	os.system("gam update group " + groupDetails[group]["email"].lower() + " sync member file \"" + config["dataFolder"] + os.sep + "Groups" + os.sep + group + ".csv\" 2>&1")
+	#print("Sync group " + group + " from CSV to GSuite.")
+	#os.system("gam update group " + groupDetails[group]["email"].lower() + " name \"" + group + "\" 2>&1")
+	#os.system("gam update group " + groupDetails[group]["email"].lower() + " sync member file \"" + config["dataFolder"] + os.sep + "Groups" + os.sep + group + ".csv\" 2>&1")
 	
 # Write out the All Pupils CSV file.
 installLib.writeFile(config["dataFolder"] + os.sep + "Groups" + os.sep + "Pupils.csv", allPupils)
-os.system("gam update group pupils@knightsbridgeschool.com name pupils 2>&1")
-os.system("gam update group pupils@knightsbridgeschool.com sync member file \"" + config["dataFolder"] + os.sep + "Groups" + os.sep + "Pupils.csv\" 2>&1")
+#os.system("gam update group pupils@knightsbridgeschool.com name pupils 2>&1")
+#os.system("gam update group pupils@knightsbridgeschool.com sync member file \"" + config["dataFolder"] + os.sep + "Groups" + os.sep + "Pupils.csv\" 2>&1")
 	
 # Read the existing basic staff details.
 staff = pandas.read_csv(config["dataFolder"] + os.sep + "staff.csv", header=0)
@@ -89,4 +89,4 @@ for staffIndex, staff in staff.iterrows():
 		outputString = outputString + str(staff["Username"]).lower() + "@knightsbridgeschool.com\n"
 installLib.writeFile(config["dataFolder"] + os.sep + "Groups" + os.sep + "Staff.csv", outputString)
 print("Sync group Staff from CSV to GSuite.")
-os.system("gam update group staff@knightsbridgeschool.com sync member file \"" + config["dataFolder"] + os.sep + "Groups" + os.sep + "Staff.csv\" 2>&1")					
+#os.system("gam update group staff@knightsbridgeschool.com sync member file \"" + config["dataFolder"] + os.sep + "Groups" + os.sep + "Staff.csv\" 2>&1")					
