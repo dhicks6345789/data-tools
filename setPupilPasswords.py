efe5e4a694c51e220c3b80307d94e0f7de8783d7
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-04-02 18:00:58 +0100

    Update setPupilPasswords.py

--- setPupilPasswords.py
+++ setPupilPasswords.py
@@ -55,7 +55,7 @@ def setPassword(theUser, thePassword):
 			# os.system("gam update user " + theUser + "@knightsbridgeschool.com password " + thePassword)
 			for emailAddress in str(pupilEntry["Contacts"]).split(" "):
 				# from itsupport@knightsbridgeschool.com replyto itsupport@knightsbridgeschool.com
-				emailAddress = "d.hicks@knightsbridgeschool.com"
+				emailAddress = "ukfootprint@gmail.com"
 				os.system("gam sendemail " + emailAddress + " subject \"Knightsbridge School - Pupil Account Password Reset\" file fullEmailTemplate.html replace childName \"" + pupilEntry["GivenName"] + " " + pupilEntry["FamilyName"] + "\" replace childUsername " + pupilEntry["OldUsername"] + " replace childPassword " + thePassword + " replace name \"David Hicks\" replace title \"IT Helpdesk\" replace email ithelpdesk@knightsbridgeschool.com replace telephone \"020 7590 9004\" html 2>&1")
 
 # Read the Forms and Yeargroups into one list.
