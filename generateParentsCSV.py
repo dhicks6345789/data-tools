399f18b19e352273b7e264cdd36aae067068c853
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-04-02 16:44:48 +0100

    Update generateParentsCSV.py

--- generateParentsCSV.py
+++ generateParentsCSV.py
@@ -29,7 +29,7 @@ def multisplit(theString, theSplit):
 		for resultItem in result:
 			for splitItem in resultItem.split(theSplit[pl]):
 				tempResult.append(splitItem)
-				print "one"
+				print(tempResult)
 		result = tempResult
 	return result
 
