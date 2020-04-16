8b18c6793591c44fcaa58e3220e1c31920beb92c
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-04-16 14:18:45 +0100

    Update processClubsEmails.py

--- processClubsEmails.py
+++ processClubsEmails.py
@@ -23,7 +23,7 @@ for requiredConfigParameter in requiredConfigParameters:
 os.makedirs(config["dataFolder"] + os.sep + "Clubs", exist_ok=True)
 os.makedirs(config["dataFolder"] + os.sep + "Clubs" + os.sep + "Emails", exist_ok=True)
 
-for email in csv.DictReader(runCommand("gam user f.hall print messages query \"newer_than:4m AND from:no-reply@squarespace.com AND subject:'Knightsbridge School: A New Order has Arrived'\" max_to_print 1")):
+for email in csv.DictReader(runCommand("gam user f.hall print messages query \"newer_than:4m AND from:no-reply@squarespace.com AND subject:'Knightsbridge School: A New Order has Arrived'\" max_to_print 5")):
 	filenamePath = config["dataFolder"] + os.sep + "Clubs" + os.sep + "Emails" + os.sep + email["id"] + ".txt"
 	if not os.path.exists(filenamePath):
 		for emailWithBody in csv.DictReader(runCommand("gam user f.hall print messages ids " + email["id"] + " showbody")):
