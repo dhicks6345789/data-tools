a444841548468de5f2d30ce4b31f788a11dccb0e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-04-16 12:52:27 +0100

    Create processClubsEmails.py

---
+++ processClubsEmails.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import os
+import re
+import sys
+import csv
+import json
+import pandas
+import installLib
+
+requiredConfigParameters = ["dataFolder"]
+
+# Load the configuration file.
+config = json.loads(installLib.readFile("config/config.json"))
+for requiredConfigParameter in requiredConfigParameters:
+	if not requiredConfigParameter in config.keys():
+		print("Error - required value " + requiredConfigParameter + " not set in config.json.")
+		sys.exit(1)
+    
+print("Hello, world! - process emails")
