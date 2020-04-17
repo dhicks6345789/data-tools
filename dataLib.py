c277e660b8119c2b116a3c894614a8a293a39f94
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-04-17 14:23:47 +0100

    Update dataLib.py

--- dataLib.py
+++ dataLib.py
@@ -1,17 +1,23 @@
+#!/usr/bin/python
 import os
 
+# A set of utility functions to help with general data manipulation.
+
+# Reads the given file, returns the entire contents as a single string.
 def readFile(theFilename):
 	inHandle = open(theFilename)
 	result = inHandle.read()
 	inHandle.close()
 	return result
 
+# Runs the given command, returns the entire output as an array of strings.
 def runCommand(theCommand):
 	commandHandle = os.popen(theCommand)
 	result = commandHandle.readlines()
 	commandHandle.close()
 	return result
 
+# Takes the given string and returns a copy with any blank lines removed, also removes any whitespace and the start and end of each line.
 def removeBlanks(theString):
 	result = ""
 	for line in theString.split("\n"):
@@ -19,3 +25,10 @@ def removeBlanks(theString):
 		if not line == "":
 			result = result + line + "\n"
 	return result.strip()
+
+# Load the configuration file.
+config = json.loads(dataLib.readFile("config/config.json"))
+for requiredConfigParameter in requiredConfigParameters:
+	if not requiredConfigParameter in config.keys():
+		print("Error - required value " + requiredConfigParameter + " not set in config.json.")
+		sys.exit(1)
