e6bfb04ee4370523ceecab5e015d27582c0d9a99
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-04-17 14:45:12 +0100

    Update processClubsEmails.py

--- processClubsEmails.py
+++ processClubsEmails.py
@@ -14,7 +14,7 @@ emailsRoot = clubsRoot + os.sep + "Emails"
 os.makedirs(emailsRoot, exist_ok=True)
 
 options = pandas.read_excel(clubsRoot + os.sep + "options.xlsx")
-print(options.at[1, "A"])
+print(options.at["A", 1])
 sys.exit(0)
 
 for email in csv.DictReader(runCommand("gam user f.hall print messages query \"newer_than:6m AND from:no-reply@squarespace.com AND subject:'Knightsbridge School: A New Order has Arrived'\"").split("\n")):
