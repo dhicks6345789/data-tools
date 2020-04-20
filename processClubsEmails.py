b4d704b784f81cc48b5aa1cbc16b5e31397a5f07
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-04-20 14:14:03 +0100

    Update processClubsEmails.py

--- processClubsEmails.py
+++ processClubsEmails.py
@@ -27,7 +27,7 @@ os.makedirs(emailsRoot, exist_ok=True)
 options = {}
 optionsDataframe = pandas.read_excel(clubsRoot + os.sep + "options.xlsx", header=None)
 for optionIndex, optionValue in optionsDataframe.iterrows():
-	print optionIndex
+	print(optionIndex)
 	options[optionsDataframe.at[optionIndex, 0].replace(":","").strip()] = optionsDataframe.at[optionIndex, 1]
 print(options)
 
