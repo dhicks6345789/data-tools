7c2351e526760e950aecc5093f5b27f6874e5300
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-04-23 12:39:04 +0100

    Update generateMathleticsCSV.py

--- generateMathleticsCSV.py
+++ generateMathleticsCSV.py
@@ -10,17 +10,17 @@ import dataLib
 config = dataLib.loadConfig(["dataFolder"])
 
 # Make sure the output folder exists.
-outputRoot = config["dataFolder"] + os.sep + "GalorePark"
+outputRoot = config["dataFolder"] + os.sep + "Mathletics"
 	
 # Input data headings:
 # Pupils: GUID,UserCode,GivenName,FamilyName,DateOfBirth,Gender,Username,YearGroup,Form,Tutor
 # Staff: GUID,UserCode,Title,GivenName,FamilyName,DateOfBirth,Username,Identifier,Form,JobTitle
-# Example output:
-# Action *,User ID,Type *,Username/Email *,Password,First name *,Middle name,Last name *,UPN,Access to application (Y/N) *
-outputString = ""
+# Output in Excel spreadsheet:
+# Student First Name (Mandatory), Student Surname (Mandatory), Student Year (Mandatory), Class Name (Mandatory), Teacher Title (Optional), Teacher First name (Mandatory), Teacher Surname (Mandatory), Teacher Email (Mandatory)
+mathletics = pandas.DataFrame(columns=["Student First Name","Student Surname","Student Year","Class Name","Teacher Title","Teacher First name","Teacher Surname","Teacher Email"])
 
 pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
-for pupilIndex, pupil in pupils.iterrows():
-	#outputString = outputString + "P" + padString(str(pupil["ID"])) + "," + pupil["Username"] + ",Student,Library,OPAC Only,Pupils," + pupil["GivenName"] + "," + pupil["FamilyName"]+ "," + pupil["Form"]+ "," + str(pupil["DateOfBirth"]) + "\n"
-	outputString = outputString + "action," + str(pupil["ID"]) + ",Type," + pupil["Username"] + ",Password," + pupil["GivenName"] + ",," + pupil["FamilyName"] + ",UPN,Y"
-installLib.writeFile(coutputRoot + os.sep + "galorePark.csv", outputString)
+#for pupilIndex, pupil in pupils.iterrows():
+#	outputString = outputString + "action," + str(pupil["ID"]) + ",Type," + pupil["Username"] + ",Password," + pupil["GivenName"] + ",," + pupil["FamilyName"] + ",UPN,Y"
+
+mathletics.to_excel(outputRoot, index=False)
