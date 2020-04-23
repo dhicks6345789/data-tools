685ee0543ab0f85fd6d204a70f9c8bcc8ca69f91
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-04-23 10:34:30 +0100

    Create syncClassroomsToWithGroups.py

---
+++ syncClassroomsToWithGroups.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import os
+import re
+import io
+import sys
+import pandas
+import dataLib
+
+# A function to remove "nan" strings from data - /really/ shouldn't be needed...
+def noNan(theString):
+	if str(theString) == "nan" or str(theString) == "0":
+		return ""
+	return str(theString.strip())
+
+# Load the config file (set by the system administrator).
+config = dataLib.loadConfig(["dataFolder"])
+
+# Load the "classroomsToSync" spreadsheet. Should simply consist of two columns, one listing Classrooms (by title), the other the groups(s)
+# to sync with that Classroom.
+classroomsDataframe = pandas.read_excel(config["dataFolder"] + os.sep + "classroomsToSync.xlsx", header=None)
+for classroomIndex, classroomValue in classroomDataframe.iterrows():
+	if not classroomIndex == 0:
+		classroomName = noNan(optionsDataframe.at[classroomIndex, 0])
+		if not classroomName == "":
+			classroomGroup = optionsDataframe.at[classroomIndex, 1]
