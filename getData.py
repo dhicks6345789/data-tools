6e9a9484ce044270695d41ac41702b70b82d3e82
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-04-25 15:31:24 +0100

    Update getData.py

--- getData.py
+++ getData.py
@@ -19,6 +19,9 @@ print("Getting data from iSAMS.")
 response = urllib.request.urlopen("https://" + config["iSAMSAPIDomain"] + "/api/batch/1.0/xml.ashx?apiKey=" + config["iSAMSAPIKey"], context=ssl._create_unverified_context())
 installLib.writeFile("iSAMSData.xml", str(response.read())[2:-1])
 
+print("Getting users list from GSuite.")
+os.system("gam print users allfields > \"" + config["dataFolder"] + os.sep + "users.csv\"")
+
 # Get a list of all courses, output in CSV format directly from GAM.
 print("Getting course list from Google Classroom.")
 os.system("gam print courses > \"" + config["dataFolder"] + os.sep + "courses.csv\"")
