8ab420bf1718ee5dabad23c947f7b526a8f05f55
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-04-27 11:38:11 +0100

    Update inviteGuardians.py

--- inviteGuardians.py
+++ inviteGuardians.py
@@ -20,7 +20,7 @@ config = dataLib.loadConfig(["dataFolder"])
 
 def inviteGuardian(theUsername, theGuardian):
 	print("Sending invite for " + theUsername)
-	os.system("gam create guardianinvite " + str(theGuardian) + " " + theUsername + "@knightsbridgeschool.com")
+	print("gam create guardianinvite " + str(theGuardian) + " " + theUsername + "@knightsbridgeschool.com")
 
 # Read the users data. Used for cross-referencing Google IDs with usernames.
 users = pandas.read_csv(config["dataFolder"] + os.sep + "users.csv", header=0)	
@@ -37,34 +37,12 @@ if len(sys.argv) == 2 and sys.argv[1] == "-getData":
 				guardians.at[guardiansIndex, "studentEmail"] = usersValue["primaryEmail"]
 	guardians.to_csv(config["dataFolder"] + os.sep + "guardians.csv", index=False)
 
-	#guardianInvitations = pandas.read_csv(io.StringIO(dataLib.runCommand("gam print guardians invitations")))
-	#for guardianInvitationsIndex, guardianInvitationsValue in guardianInvitations.iterrows():
-	#	for usersIndex, usersValue in users.iterrows():
-	#		if usersValue["id"] == guardianInvitationsValue["studentId"]:
-	#			guardianInvitations.at[guardianInvitationsIndex, "studentEmail"] = usersValue["primaryEmail"]
-	#guardianInvitations.to_csv(config["dataFolder"] + os.sep + "guardianInvitations.csv", index=False)
-
-# Read the existing Guardians list, i.e. accounts that have confirmed Guardians. Fields:
-# studentEmail,studentId,invitedEmailAddress,guardianId,guardianProfile.emailAddress,guardianProfile.id,guardianProfile.name.familyName,guardianProfile.name.fullName,guardianProfile.name.givenName,guardianProfile.photoUrl
+# Read the existing Guardians list. Fields:
+# studentEmail,studentId,invitedEmailAddress,invitationId,creationTime,state
 guardians = pandas.read_csv(config["dataFolder"] + os.sep + "guardians.csv", header=0)
 
-if len(sys.argv) >= 3:
-	if len(sys.argv) == 3 and sys.argv[1] == "-user":
-		username = sys.argv[2]
-		for pupilIndex, pupilEntry in pupils.iterrows():
-			if pupilEntry["OldUsername"] == username:
-				inviteGuardian(username, pupilEntry["MainContact"])
-	elif len(sys.argv) == 3 and sys.argv[1] == "-group":
-		groupname = sys.argv[2]
-		for pupilsIndex, pupilsValue in pupils.iterrows():
-			if groupname in pupilsValue["Form"]:
-				for contact in noNan(pupilsValue["Contacts"]).split(" "):
-					contact = contact.strip()
-					if not contact == "":
-						sendInviate = True
-						for guardiansIndex, guardiansValue in guardians.iterrows():
-							if pupilsValue["OldUsername"] == guardiansValue["studentEmail"].split("@")[0]:
-								print("Already has Guardian: " + pupilsValue["OldUsername"])
-								sendInviate = False
-					if sendInviate:
-						inviteGuardian(pupilsValue["OldUsername"], contact)
+invitedEmailAddresses = guardians["invitedEmailAddress"].tolist()
+for pupilsIndex, pupilsValue in pupils.iterrows():
+	for contact in noNan(pupilsValue["Contacts"]).split(" "):
+		if not contact in invitedEmailAddresses:
+			inviteGuardian(pupilsValue["OldUsername"], contact)
