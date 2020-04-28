a842bba5760c8baf0299e2b37c9d0cd66f3108f8
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-04-28 15:35:49 +0100

    Update syncClassroomsWithGroups.py

--- syncClassroomsWithGroups.py
+++ syncClassroomsWithGroups.py
@@ -16,13 +16,17 @@ def noNan(theString):
 config = dataLib.loadConfig(["dataFolder"])
 
 groupsRoot = config["dataFolder"] + os.sep + "Groups"
+classroomsRoot = config["dataFolder"] + os.sep + "Classrooms"
+os.makedirs(classroomsRoot, exist_ok=True)
+cacheRoot = classroomsRoot + os.sep + "CSVs"
+os.makedirs(cacheRoot, exist_ok=True)
 
 # Get a current list of Google Classrooms.
 classrooms =  pandas.read_csv(io.StringIO(dataLib.runCommand("gam print courses")))
 
 # Load the "classroomsToSync" spreadsheet. Should simply consist of two columns, one listing Classrooms (by title), the other the groups(s)
 # to sync with that Classroom.
-classroomsDataframe = pandas.read_excel(config["dataFolder"] + os.sep + "classroomsToSync.xlsx", header=None)
+classroomsDataframe = pandas.read_excel(classroomsRoot + os.sep + "classroomsToSync.xlsx", header=None)
 for classroomIndex, classroomValue in classroomsDataframe.iterrows():
 	if not classroomIndex == 0:
 		classroomName = noNan(classroomsDataframe.at[classroomIndex, 0])
@@ -36,14 +40,15 @@ for classroomIndex, classroomValue in classroomsDataframe.iterrows():
 					pupils = pupils + dataLib.readFile(csvPath)
 				else:
 					print("Unknown group: " + pupilsGroup.strip())
-			if not pupils == "":
+			#if not pupils == "":
+			if dataLib.rewriteCachedData(cacheRoot + os.sep + classroomName + ".csv", pupils):
 				dataLib.writeFile("pupilsData.csv", pupils)
 				classroomID = ""
 				for classroomIndex, classroomValue in classrooms.iterrows():
 					if classroomValue["courseState"] == "ACTIVE" and classroomValue["name"] == classroomName:
 						print("Syncing: " + classroomValue["name"])
 						classroomID = noNan(str(classroomValue["id"]))
-						os.system("gam course " + classroomID + " sync students file pupilsData.csv")
+						print("gam course " + classroomID + " sync students file pupilsData.csv")
 				os.remove("pupilsData.csv")
 			if not teachers == "":
 				dataLib.writeFile("teachersData.csv", teachers.replace(",","\n").strip())
