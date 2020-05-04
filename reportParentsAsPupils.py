9f44402dda4ac8a03e9ccca4b7516e30bc377f24
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-05-04 13:59:04 +0100

    Update reportParentsAsPupils.py

--- reportParentsAsPupils.py
+++ reportParentsAsPupils.py
@@ -24,7 +24,10 @@ courses = pandas.read_csv(config["dataFolder"] + os.sep + "courses.csv", header=
 
 for coursesIndex, coursesValues in courses.iterrows():
 	if str(coursesValues["id"]) == "99103269116":
+		# CSV Format from GAM report:
+		# courseId,courseName,userRole,userId,profile.emailAddress,profile.id,profile.name.familyName,profile.name.fullName,profile.name.givenName,profile.permissions,profile.permissions.0.permission,profile.photoUrl,profile.verifiedTeacher
 		members = pandas.read_csv(io.StringIO(dataLib.runCommand("gam print course-participants course " + str(coursesValues["id"]) + " show all")))
-		print(members)
+		for membersIndex, membersValues in members.iterrows():
+			print(membersValues["profile.emailAddress"])
 	
 parentsAsPupils.to_csv(outputRoot + os.sep + "report.csv", index=False)
