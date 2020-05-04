a9b9262c5737b46923ad6ef7f96c2f3012089d2e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-05-04 14:02:36 +0100

    Update reportParentsAsPupils.py

--- reportParentsAsPupils.py
+++ reportParentsAsPupils.py
@@ -22,12 +22,17 @@ parentsAsPupils = pandas.DataFrame(columns=["First Name","Last Name","Email","Cl
 parents = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
 courses = pandas.read_csv(config["dataFolder"] + os.sep + "courses.csv", header=0)
 
+resultIndex = 0
+count = 0
 for coursesIndex, coursesValues in courses.iterrows():
-	if str(coursesValues["id"]) == "99103269116":
+	count = count + 1
+	if count < 10:
 		# CSV Format from GAM report:
 		# courseId,courseName,userRole,userId,profile.emailAddress,profile.id,profile.name.familyName,profile.name.fullName,profile.name.givenName,profile.permissions,profile.permissions.0.permission,profile.photoUrl,profile.verifiedTeacher
 		members = pandas.read_csv(io.StringIO(dataLib.runCommand("gam print course-participants course " + str(coursesValues["id"]) + " show all")))
 		for membersIndex, membersValues in members.iterrows():
-			print(membersValues["profile.emailAddress"])
+			if not membersValues["profile.emailAddress"].endswith("@knightsbridgeschool.com"):
+				parentsAsPupils.at[resultIndex, "Email"] = membersValues["profile.emailAddress"]
+				resultIndex = resultIndex + 1
 	
 parentsAsPupils.to_csv(outputRoot + os.sep + "report.csv", index=False)
