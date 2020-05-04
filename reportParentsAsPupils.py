500e1a8d944caafc8cd0687d7bd966495b23ecb4
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-05-04 14:08:08 +0100

    Update reportParentsAsPupils.py

--- reportParentsAsPupils.py
+++ reportParentsAsPupils.py
@@ -17,7 +17,7 @@ os.makedirs(outputRoot, exist_ok=True)
 # Input data headings:
 # Parents:
 # Output in CSV format:
-parentsAsPupils = pandas.DataFrame(columns=["First Name","Last Name","Email","Classroom","Classroom Teachers"])
+parentsAsPupils = pandas.DataFrame(columns=["First Name","Last Name","Email","Classroom Name","Classroom ID","Classroom Teachers"])
 
 parents = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
 courses = pandas.read_csv(config["dataFolder"] + os.sep + "courses.csv", header=0)
@@ -26,13 +26,15 @@ resultIndex = 0
 count = 0
 for coursesIndex, coursesValues in courses.iterrows():
 	count = count + 1
-	if count < 10:
+	if count < 100:
 		# CSV Format from GAM report:
 		# courseId,courseName,userRole,userId,profile.emailAddress,profile.id,profile.name.familyName,profile.name.fullName,profile.name.givenName,profile.permissions,profile.permissions.0.permission,profile.photoUrl,profile.verifiedTeacher
 		members = pandas.read_csv(io.StringIO(dataLib.runCommand("gam print course-participants course " + str(coursesValues["id"]) + " show all")))
 		for membersIndex, membersValues in members.iterrows():
 			if not membersValues["profile.emailAddress"].endswith("@knightsbridgeschool.com"):
 				parentsAsPupils.at[resultIndex, "Email"] = membersValues["profile.emailAddress"]
+				parentsAsPupils.at[resultIndex, "Classroom Name"] = str(coursesValues["name"])
+				parentsAsPupils.at[resultIndex, "Classroom ID"] = str(coursesValues["id"])
 				resultIndex = resultIndex + 1
 	
 parentsAsPupils.to_csv(outputRoot + os.sep + "report.csv", index=False)
