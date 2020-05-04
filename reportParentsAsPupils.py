ef56d5606c0e9282cdd03a74dba23f66f34c3587
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-05-04 12:46:51 +0100

    Update reportParentsAsPupils.py

--- reportParentsAsPupils.py
+++ reportParentsAsPupils.py
@@ -5,42 +5,22 @@ import json
 import pandas
 import dataLib
 
-parentEmailYeargroups = ["3","4"]
-ownEmailYearGroups = ["5","6","7","8"]
-
-def formToYearGroup(theForm):
-	for validYeargroup in parentEmailYeargroups + ownEmailYearGroups:
-		if validYeargroup in theForm:
-			return validYeargroup.replace("Rec","R")
-	return None
-
-yearGroupToLevel = {"3":"1","4":"1","5":"2","6":"2","7":"3","8":"3"}
-
 # Load the config file (set by the system administrator).
 config = dataLib.loadConfig(["dataFolder"])
 
 # Make sure the output folder exists.
-outputRoot = config["dataFolder"] + os.sep + "FirstNews"
+reportsRoot = config["dataFolder"] + os.sep + "Reports"
+outputRoot reportsRoot + os.sep + "Parents Joined To Classrooms As Pupils"
 os.makedirs(outputRoot, exist_ok=True)
 
 # Input data headings:
-# Pupils: GUID,UserCode,GivenName,FamilyName,DateOfBirth,Gender,Username,YearGroup,Form,Tutor
+# Parents:
 # Output in CSV format:
-# First Name,Last Name,"Level (1, 2 or 3)",Parent/Student Email
-# Delete,Me,1,pupil@exampleemail.com
-firstNews = pandas.DataFrame(columns=["First Name","Last Name","Level","Email"])
+parentsAsPupils = pandas.DataFrame(columns=["First Name","Last Name","Email","Classroom","Classroom Teachers"])
 
-pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
+parents = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
 
-for pupilsIndex, pupilsValues in pupils.iterrows():
-	yearGroup = formToYearGroup(pupilsValues["Form"])
-	if not yearGroup == None:
-		firstNews.at[pupilsIndex+1, "First Name"] = pupilsValues["GivenName"]
-		firstNews.at[pupilsIndex+1, "Last Name"] = pupilsValues["FamilyName"]
-		firstNews.at[pupilsIndex+1, "Level"] = yearGroupToLevel[yearGroup]
-		if yearGroup in ownEmailYearGroups:
-			firstNews.at[pupilsIndex+1, "Email"] = pupilsValues["OldUsername"] + "@knightsbridgeschool.com"
-		else:
-			firstNews.at[pupilsIndex+1, "Email"] = pupilsValues["Contacts"].split(" ")[0]
+#for parentsIndex, parentsValues in parents.iterrows():
+#	parentsValues["email"]
 
-firstNews.to_csv(outputRoot + os.sep + "FirstNews.csv", index=False)
+parentsAsPupils.to_csv(outputRoot + os.sep + "report.csv", index=False)
