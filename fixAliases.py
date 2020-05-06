91a72c2a50a1c15793ffb1e357cc52be1a6aecc8
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-05-06 14:11:16 +0100

    Update fixAliases.py

--- fixAliases.py
+++ fixAliases.py
@@ -10,27 +10,11 @@ config = dataLib.loadConfig(["dataFolder"])
 
 # Make sure the output folder exists.
 reportsRoot = config["dataFolder"] + os.sep + "Reports"
-outputRoot = reportsRoot + os.sep + "Pupil Engagement"
 os.makedirs(outputRoot, exist_ok=True)
 
 pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
 activity = pandas.read_csv(config["dataFolder"] + os.sep + "Reports" + os.sep + "userActivity.csv", header=0)
 
-report = pandas.DataFrame(columns=["Name","Username","Yeargroup","ClassroomLastInteractionTime"])
-
-yearGroups = {}
-for pupilsIndex, pupilsValues in pupils.iterrows():
-	yearGroups[pupilsValues["YearGroup"]] = 1
-reportIndex = 0
-for yearGroup in yearGroups.keys():
-	for pupilsIndex, pupilsValues in pupils.iterrows():
-		if pupilsValues["YearGroup"] == yearGroup:
-			for activityIndex, activityValues in activity.iterrows():
-				if activityValues["email"] == pupilsValues["OldUsername"] + "@knightsbridgeschool.com":
-					report.at[reportIndex, "Name"] = pupilsValues["GivenName"] + " " + pupilsValues["FamilyName"]
-					report.at[reportIndex, "Username"] = pupilsValues["OldUsername"]
-					report.at[reportIndex, "Yeargroup"] = yearGroup
-					report.at[reportIndex, "ClassroomLastInteractionTime"] = activityValues["classroom:last_interaction_time"]
-					reportIndex = reportIndex + 1
-
-report.to_csv(outputRoot + os.sep + "report.csv", index=False)
+for activityIndex, activityValues in activity.iterrows():
+	print(activityValues["email"])
+	#pupilsValues["OldUsername"] + "@knightsbridgeschool.com":
