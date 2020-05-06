edef4df64bd12db9af0af607ceac6011280565c2
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-05-06 12:54:41 +0100

    Update reportPupilEngagement.py

--- reportPupilEngagement.py
+++ reportPupilEngagement.py
@@ -17,16 +17,21 @@ os.makedirs(outputRoot, exist_ok=True)
 pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
 activity = pandas.read_csv(config["dataFolder"] + os.sep + "Reports" + os.sep + "userActivity.csv", header=0)
 
-#resultIndex = 0
-#count = 0
+result = pandas.DataFrame(columns=["Name","Username","Yeargroup"])
+
 yearGroups = {}
 for pupilsIndex, pupilsValues in pupils.iterrows():
 	yearGroups[pupilsValues["YearGroup"]] = 1
+resultIndex = 0
 for yearGroup in yearGroups.keys():
 	for pupilsIndex, pupilsValues in pupils.iterrows():
 		if pupilsValues["YearGroup"] == yearGroup:
 			for activityIndex, activityValues in activity.iterrows():
 				if activityValues["email"] == pupilsValues["OldUsername"] + "@knightsbridgeschool.com":
-					print(activityValues["email"])
+					result.at[resultIndex, "Name"] = pupilsValues["GivenName"] + " " + pupilsValues["FamilyName"]
+					result.at[resultIndex, "Username"] = pupilsValues["OldUsername"]
+					result.at[resultIndex, "Yeargroup"] = yearGroup
+					#activityValues["email"]
+					resultIndex = resultIndex + 1
 
-#parentsAsPupils.to_csv(outputRoot + os.sep + "report.csv", index=False)
+engagement.to_csv(outputRoot + os.sep + "report.csv", index=False)
