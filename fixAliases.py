f4ff64bdcecf02cad4af7a783c14d682d6155400
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-05-06 16:25:11 +0100

    Update fixAliases.py

--- fixAliases.py
+++ fixAliases.py
@@ -11,13 +11,12 @@ config = dataLib.loadConfig(["dataFolder"])
 # Make sure the output folder exists.
 reportsRoot = config["dataFolder"] + os.sep + "Reports"
 
-pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
-oldPupilUsernames = pupils["OldUsername"].tolist()
-newPupilUsernames = pupils["Username"].tolist()
-
 activity = pandas.read_csv(config["dataFolder"] + os.sep + "Reports" + os.sep + "userActivity.csv", header=0)
+pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
 
 for activityIndex, activityValues in activity.iterrows():
 	username = activityValues["email"].split("@")[0]
-	if username in newPupilUsernames and username.endswith("17") and activityValues["accounts:last_login_time"] == "Never":
-		print("gam delete user " + username)
+	for pupilsIndex, pupilsValues in pupils.iterrows():
+		if pupilsValues["OldUsername"] == username and username.endswith("17") and activityValues["accounts:last_login_time"] == "Never":
+			print("gam delete user " + username)
+			print("gam make alias for " + pupilsValues["Username"] + " as " + username)
