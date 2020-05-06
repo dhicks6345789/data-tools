5117ffe307413cf8f0cecaeff6781b1fa09f4099
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-05-06 12:57:37 +0100

    Update reportPupilEngagement.py

--- reportPupilEngagement.py
+++ reportPupilEngagement.py
@@ -17,21 +17,21 @@ os.makedirs(outputRoot, exist_ok=True)
 pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
 activity = pandas.read_csv(config["dataFolder"] + os.sep + "Reports" + os.sep + "userActivity.csv", header=0)
 
-result = pandas.DataFrame(columns=["Name","Username","Yeargroup"])
+report = pandas.DataFrame(columns=["Name","Username","Yeargroup"])
 
 yearGroups = {}
 for pupilsIndex, pupilsValues in pupils.iterrows():
 	yearGroups[pupilsValues["YearGroup"]] = 1
-resultIndex = 0
+reportIndex = 0
 for yearGroup in yearGroups.keys():
 	for pupilsIndex, pupilsValues in pupils.iterrows():
 		if pupilsValues["YearGroup"] == yearGroup:
 			for activityIndex, activityValues in activity.iterrows():
 				if activityValues["email"] == pupilsValues["OldUsername"] + "@knightsbridgeschool.com":
-					result.at[resultIndex, "Name"] = pupilsValues["GivenName"] + " " + pupilsValues["FamilyName"]
-					result.at[resultIndex, "Username"] = pupilsValues["OldUsername"]
-					result.at[resultIndex, "Yeargroup"] = yearGroup
+					report.at[reportIndex, "Name"] = pupilsValues["GivenName"] + " " + pupilsValues["FamilyName"]
+					report.at[reportIndex, "Username"] = pupilsValues["OldUsername"]
+					report.at[reportIndex, "Yeargroup"] = yearGroup
 					#activityValues["email"]
-					resultIndex = resultIndex + 1
+					reportIndex = resultIndex + 1
 
-engagement.to_csv(outputRoot + os.sep + "report.csv", index=False)
+report.to_csv(outputRoot + os.sep + "report.csv", index=False)
