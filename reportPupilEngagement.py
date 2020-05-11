bd45100a6b76bdb63569460db7ac76e84b65bf7d
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-05-11 23:15:19 +0100

    Update reportPupilEngagement.py

--- reportPupilEngagement.py
+++ reportPupilEngagement.py
@@ -90,12 +90,8 @@ for yearGroup in yearGroups.keys():
 						lastActivity = lastClassroomDays
 					else:
 						lastActivity = lastLoginDays
-					if lastActivity == "Never":
-						report.at[indexToUse, "Activity"] = "Never"
-					else:
-						days = (datetime.datetime.now() - datetime.datetime.strptime(lastActivity, "%Y-%m-%dT%H:%M:%S.%fZ")).days					
-						# pdfCanvas.setFillColorRGB(colourValue,1-colourValue,0)
-						report.at[indexToUse, "Activity"] = 1 - intToConstrainedPercentage(days, 3, 8)
+					report.at[indexToUse, "Activity"] = 1 - intToConstrainedPercentage(lastActivity, 3, 8)
+					# pdfCanvas.setFillColorRGB(colourValue,1-colourValue,0)
 
 # Write out the CSV report.
 report.to_csv(outputRoot + os.sep + "report.csv", index=False)
