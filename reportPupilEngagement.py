661f7e4a83d6bd8ffc73110dfde1ab1dc2adda4b
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-05-11 13:13:49 +0100

    Update reportPupilEngagement.py

--- reportPupilEngagement.py
+++ reportPupilEngagement.py
@@ -94,12 +94,9 @@ for yearGroup in yearGroups.keys():
 			for columnName in columnNames:
 				if lineNumber % 2 == 0:
 					pdfCanvas.drawInlineImage(lineImage, leftBorder*reportlab.lib.units.mm, ((pageHeight-(lineHeight*(lineNumber+1))-(int(lineHeight/4)))-topBorder)*reportlab.lib.units.mm, (pageWidth-(leftBorder*2))*reportlab.lib.units.mm, lineHeight*reportlab.lib.units.mm)
-					columnValue = str(reportValues[columnName])
-					if columnName == "Login":
-						if not columnValue == "Never":
-							columnValue = (datetime.datetime.now() - datetime.datetime.strptime(columnValue, "%Y-%m-%dT%H:%M:%S.%fZ")).days
-							columnValue = str(columnValue)
-							print(columnValue)
+				columnValue = str(reportValues[columnName])
+				if columnName == "Login" and not columnValue == "Never":
+					columnValue = str((datetime.datetime.now() - datetime.datetime.strptime(columnValue, "%Y-%m-%dT%H:%M:%S.%fZ")).days)
 				pdfCanvas.drawString((leftBorder+columnPos[columnName])*reportlab.lib.units.mm, ((pageHeight-(lineHeight*lineNumber))-topBorder)*reportlab.lib.units.mm, columnValue)
 			lineNumber = lineNumber + 1
 	# Save the PDF document.
