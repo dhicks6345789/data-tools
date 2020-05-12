fd1640b54c3a718427e160dfbb6f8ff2b35487ed
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-05-12 20:13:54 +0100

    Update reportPupilEngagement.py

--- reportPupilEngagement.py
+++ reportPupilEngagement.py
@@ -100,8 +100,8 @@ for yearGroup in yearGroups.keys():
 					if lastActive == "Never":
 						report.at[indexToUse, "Last Active"] = "Never"
 					else:
-						print(lastActive.strftime("%d/%m/%Y"))
-						report.at[indexToUse, "Last Active"] = lastActive.strftime("%d/%m/%Y")
+						#print(lastActive.strftime("%d/%m/%Y"))
+						report.at[indexToUse, "Last Active"] = str(lastActive.strftime("%d/%m/%Y"))
 					
 					#lastLoginDays = dateToDaysAgo(activityValues["accounts:last_login_time"])
 					#lastClassroomDays = dateToDaysAgo(activityValues["classroom:last_interaction_time"])
