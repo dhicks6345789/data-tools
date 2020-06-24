8111d2ee8ebb633364833f6b8e668694c9639070
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-06-24 13:01:37 +0100

    Update generatePrintedCalendar.py

--- generatePrintedCalendar.py
+++ generatePrintedCalendar.py
@@ -26,5 +26,22 @@ import reportlab.graphics.renderPM
 # Load the config file (set by the system administrator).
 config = dataLib.loadConfig(["dataFolder"])
 
+# Check the command line paramaters.
 if not len(sys.argv) == 3:
 	print("Usage: generatedPrintedCalendar URL outputFilename")
+
+# Make sure the input / output folder exists.
+calendarFolder = config["dataFolder"] + os.sep + "Calendars"
+os.makedirs(calendarsFolder, exist_ok=True)
+
+# Check to see if there is content to merge.
+if os.path.exists(calendarsFolder + os.sep + outputFilename + os.sep + "frontMatter.pdf"):
+	print("Found front matter...")
+
+if os.path.exists(calendarsFolder + os.sep + outputFilename + os.sep + "backMatter.pdf"):
+	print("Found back matter...")
+
+# Create the blank PDF document to start drawing page elements on.
+pdfCanvas = reportlab.pdfgen.canvas.Canvas(calendarsFolder + os.sep + outputFilename + ".pdf")
+# Save the PDF document.
+pdfCanvas.save()
