9d9da0961941dd9c60b424203987bbfb32cbd421
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-06-29 15:07:02 +0100

    Update generatePrintedCalendar.py

--- generatePrintedCalendar.py
+++ generatePrintedCalendar.py
@@ -45,6 +45,7 @@ def drawCalendarPage(thePDFCanvas, headings):
 	headingImage = PIL.Image.new("RGB", (pageWidth-(border*2), lineHeight), (200, 200, 200))
 	headingY = topY - lineHeight
 	for heading in headings:
+		pdfCanvas.drawInlineImage(lineImage, leftX*reportlab.lib.units.mm, headingY*reportlab.lib.units.mm, rightX*reportlab.lib.units.mm, lineHeight*reportlab.lib.units.mm)
 		thePDFCanvas.line(leftX*reportlab.lib.units.mm, headingY*reportlab.lib.units.mm, rightX*reportlab.lib.units.mm, headingY*reportlab.lib.units.mm)
 		yPos = headingY-lineHeight
 		thePDFCanvas.line(leftX*reportlab.lib.units.mm, yPos*reportlab.lib.units.mm, rightX*reportlab.lib.units.mm, yPos*reportlab.lib.units.mm)
