adcd67ae9fcbe80dd0eef21a2c6ef47644970e4c
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-06-29 15:31:38 +0100

    Update generatePrintedCalendar.py

--- generatePrintedCalendar.py
+++ generatePrintedCalendar.py
@@ -41,11 +41,11 @@ tableWidth = pageWidth-(borderSize*2)
 
 
 def drawCalendarPage(thePDFCanvas, headings):
-	cellHeight = (pageHeight - ((borderSize*2) + lineHeight)) / len(headings)
+	cellHeight = (pageHeight - ((borderSize*2) + lineHeight)) / (len(headings)-1)
 	# A mid-gray background to make following lines on the page a bit easier.
 	headingImage = PIL.Image.new("RGB", (tableWidth, lineHeight), (200, 200, 200))
 	headingY = topY - lineHeight
-	for heading in headings:
+	for heading in headings[1:]:
 		pdfCanvas.drawInlineImage(headingImage, leftX*reportlab.lib.units.mm, (headingY-lineHeight)*reportlab.lib.units.mm, tableWidth*reportlab.lib.units.mm, lineHeight*reportlab.lib.units.mm)
 		thePDFCanvas.line(leftX*reportlab.lib.units.mm, headingY*reportlab.lib.units.mm, rightX*reportlab.lib.units.mm, headingY*reportlab.lib.units.mm)
 		yPos = headingY-lineHeight
