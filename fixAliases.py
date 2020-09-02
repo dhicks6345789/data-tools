82d698256c0b024e3d6f5433526c71b7db62119e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-09-02 17:49:34 +0100

    Update fixAliases.py

--- fixAliases.py
+++ fixAliases.py
@@ -14,9 +14,12 @@ reportsRoot = config["dataFolder"] + os.sep + "Reports"
 activity = pandas.read_csv(config["dataFolder"] + os.sep + "Reports" + os.sep + "userActivity.csv", header=0)
 pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
 
-for activityIndex, activityValues in activity.iterrows():
-	username = activityValues["email"].split("@")[0]
-	for pupilsIndex, pupilsValues in pupils.iterrows():
-		if pupilsValues["Username"] == username and username.endswith("17") and activityValues["accounts:last_login_time"] == "Never":
-			print("gam delete user " + username)
-			#print("gam create alias " + username + " user " + pupilsValues["OldUsername"])
+for pupilsIndex, pupilsValues in pupils.iterrows():
+	print(pupilsValues["OldUsername"] + " to " + pupilsValues["Username"])
+
+#for activityIndex, activityValues in activity.iterrows():
+#	username = activityValues["email"].split("@")[0]
+#	for pupilsIndex, pupilsValues in pupils.iterrows():
+#		if pupilsValues["Username"] == username and username.endswith("17") and activityValues["accounts:last_login_time"] == "Never":
+#			print("gam delete user " + username)
+#			#print("gam create alias " + username + " user " + pupilsValues["OldUsername"])
