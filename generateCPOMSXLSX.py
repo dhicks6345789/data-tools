044b363e3fd9357d9a0a9d658d17a0d9350c1afe
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-09-22 11:42:29 +0100

    Update generateCPOMSXLSX.py

--- generateCPOMSXLSX.py
+++ generateCPOMSXLSX.py
@@ -18,43 +18,17 @@ def formToYearGroup(theForm):
 config = dataLib.loadConfig(["dataFolder"])
 
 # Make sure the output folder exists.
-outputRoot = config["dataFolder"] + os.sep + "Mathletics"
+outputRoot = config["dataFolder"] + os.sep + "CPOMS"
 os.makedirs(outputRoot, exist_ok=True)
 
 # Input data headings:
-# Pupils: GUID,UserCode,GivenName,FamilyName,DateOfBirth,Gender,Username,YearGroup,Form,Tutor
 # Staff: GUID,UserCode,Title,GivenName,FamilyName,DateOfBirth,Username,Identifier,Form,JobTitle
 # Output in Excel spreadsheet:
-# Student First Name (Mandatory), Student Surname (Mandatory), Student Year (Mandatory), Class Name (Mandatory), Teacher Title (Optional), Teacher First name (Mandatory), Teacher Surname (Mandatory), Teacher Email (Mandatory)
-mathletics = pandas.DataFrame(columns=["Student First Name (Mandatory)","Student Surname (Mandatory)","Student Year (Mandatory)","Class Name (Mandatory)","Teacher Title (Optional)","Teacher First name (Mandatory)","Teacher Surname (Mandatory)","Teacher Email (Mandatory)"])
-pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
-students = pandas.read_excel(outputRoot + os.sep + "Students.xlsx", header=0)
+cpoms = pandas.DataFrame(columns=["Firstname","Surname","School/Establishment Email Address","Job Title","User Group","Class Restrictions"])
 teachers = pandas.read_excel(outputRoot + os.sep + "Teachers.xlsx", header=0)
-for pupilsIndex, pupilsValues in pupils.iterrows():
-	yearGroup = formToYearGroup(pupilsValues["Form"])
-	if not yearGroup == None:
-		studentFound = False
-		for studentsIndex, studentsValues in students.iterrows():
-			if studentsValues["Student First Name (Mandatory)"] == pupilsValues["GivenName"] and studentsValues["Student Surname (Mandatory)"] == pupilsValues["FamilyName"]:
-				studentFound = True
-				mathletics.at[pupilsIndex+1, "Student First Name (Mandatory)"] = studentsValues["Student First Name (Mandatory)"]
-				mathletics.at[pupilsIndex+1, "Student Surname (Mandatory)"] = studentsValues["Student Surname (Mandatory)"]
-				mathletics.at[pupilsIndex+1, "Student Year (Mandatory)"] = yearGroup
-				mathletics.at[pupilsIndex+1, "Class Name (Mandatory)"] = studentsValues["Class Name (Mandatory)"]
-				mathletics.at[pupilsIndex+1, "Teacher Title (Optional)"] = studentsValues["Teacher Title (Optional)"]
-				mathletics.at[pupilsIndex+1, "Teacher First name (Mandatory)"] = studentsValues["Teacher First name (Mandatory)"]
-				mathletics.at[pupilsIndex+1, "Teacher Surname (Mandatory)"] = studentsValues["Teacher Surname (Mandatory)"]
-				mathletics.at[pupilsIndex+1, "Teacher Email (Mandatory)"] = studentsValues["Teacher Email (Mandatory)"]
-		if not studentFound:
-			mathletics.at[pupilsIndex+1, "Student First Name (Mandatory)"] = pupilsValues["GivenName"]
-			mathletics.at[pupilsIndex+1, "Student Surname (Mandatory)"] = pupilsValues["FamilyName"]
-			mathletics.at[pupilsIndex+1, "Student Year (Mandatory)"] = yearGroup
-			mathletics.at[pupilsIndex+1, "Class Name (Mandatory)"] = pupilsValues["Form"]
-			for teachersIndex, teachersValues in teachers.iterrows():
-				if pupilsValues["Form"] == teachersValues["Class Name"]:
-					mathletics.at[pupilsIndex+1, "Teacher Title (Optional)"] = teachersValues["Teacher Title"]
-					mathletics.at[pupilsIndex+1, "Teacher First name (Mandatory)"] = teachersValues["Teacher First name"]
-					mathletics.at[pupilsIndex+1, "Teacher Surname (Mandatory)"] = teachersValues["Teacher Surname"]
-					mathletics.at[pupilsIndex+1, "Teacher Email (Mandatory)"] = teachersValues["Teacher Email"]
-
-mathletics.to_excel(outputRoot + os.sep + "Mathletics.xlsx", index=False)
+for teachersIndex, teachersValues in teachers.iterrows():
+	cpoms.at[teachersIndex+1, "Firstname"] = teachersValues["GivenName"]
+	cpoms.at[teachersIndex+1, "Surname"] = teachersValues["FamilyName"]
+	cpoms.at[teachersIndex+1, "School/Establishment Email Address"] = teachersValues["Username"] + "@knightsbridgeschool.com"
+		
+mathletics.to_excel(outputRoot + os.sep + "CPOMS.xlsx", index=False)
