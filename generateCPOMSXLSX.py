02ef082962ab47abd00e5d8be0ced84e7a08dc4d
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-09-22 12:33:03 +0100

    Update generateCPOMSXLSX.py

--- generateCPOMSXLSX.py
+++ generateCPOMSXLSX.py
@@ -33,11 +33,11 @@ for staffIndex, staffValues in staff.iterrows():
 	cpoms.at[staffIndex+1, "Surname"] = staffValues["FamilyName"]
 	cpoms.at[staffIndex+1, "School/Establishment Email Address"] = staffValues["Username"] + "@knightsbridgeschool.com"
 	cpoms.at[staffIndex+1, "Job Title"] = staffValues["JobTitle"]
-	jobTitleHandled = false
+	jobTitleHandled = False
 	for groupTitle in groups.keys():
 		for keyword in groups[groupTitle]:
 			if keyword in cellToStr(staffValues["JobTitle"]).lower():
-				jobTitleHandled = true
+				jobTitleHandled = True
 				cpoms.at[staffIndex+1, "User Group"] = groupTitle
 	if not jobTitleHandled:
 		print("Unhandled job description: " + staffValues["JobTitle"])
