e0bb91f963d54aea1d961a4c41999591c95c71e3
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-10-06 12:43:40 +0100

    Update checkPupilOUs.py

--- checkPupilOUs.py
+++ checkPupilOUs.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python
 import os
-#import re
+import re
 import io
-#import sys
-#import csv
-#import json
 import pandas
 import dataLib
 
 # Load the config file (set by the system administrator).
 config = dataLib.loadConfig(["dataFolder"])
 
-# Read the existing basic pupils data.
-#pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
-#for pupilsIndex, pupilsValue in pupils.iterrows():
-
 usernames = pandas.read_csv(io.StringIO(dataLib.runCommand("gam print users query \"orgUnitPath='/KS-Pupils'\"")))
 for usernamesIndex, usernamesValue in usernames.iterrows():
-  print(usernamesValue["primaryEmail"])
+  usernameMatch = re.match("(.*\..*)(\d\d)@knightsbridgeschool.com", usernamesValue["primaryEmail"])
+  if not usernameMatch = None:
+    print(usrnameMatch.group(1))
+    print(usrnameMatch.group(2))
