e4f3fea12f84a05be577d3a0d90fbcab2976cc16
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-10-06 12:29:44 +0100

    Update checkPupilOUs.py

--- checkPupilOUs.py
+++ checkPupilOUs.py
@@ -1,1 +1,17 @@
-print("")
+#!/usr/bin/python
+import os
+#import re
+#import io
+#import sys
+#import csv
+#import json
+#import pandas
+import dataLib
+
+# Load the config file (set by the system administrator).
+config = dataLib.loadConfig(["dataFolder"])
+
+# Read the existing basic pupils data.
+pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
+for pupilsIndex, pupilsValue in pupils.iterrows():
+  print(pupilsValue["Username"])
