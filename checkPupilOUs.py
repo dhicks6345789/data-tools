54829d27f7dc2dab649439f33333bc54b3f54428
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-10-06 12:36:46 +0100

    Update checkPupilOUs.py

--- checkPupilOUs.py
+++ checkPupilOUs.py
@@ -12,6 +12,9 @@ import dataLib
 config = dataLib.loadConfig(["dataFolder"])
 
 # Read the existing basic pupils data.
-pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
-for pupilsIndex, pupilsValue in pupils.iterrows():
-  print(pupilsValue["Username"])
+#pupils = pandas.read_csv(config["dataFolder"] + os.sep + "pupils.csv", header=0)
+#for pupilsIndex, pupilsValue in pupils.iterrows():
+
+usernames = pandas.read_csv(io.StringIO(dataLib.runCommand("gam print users query \"orgUnitPath='/KS-Pupils'\"")))
+for usernamesIndex, usernamesValue in pupils.iterrows():
+  print(usernamesValue[0])
