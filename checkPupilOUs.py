092bedde9cf76263d046369bfddd421dac592056
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-10-12 16:15:49 +0100

    Update checkPupilOUs.py

--- checkPupilOUs.py
+++ checkPupilOUs.py
@@ -8,10 +8,11 @@ import dataLib
 # Load the config file (set by the system administrator).
 config = dataLib.loadConfig(["dataFolder"])
 
-usernames = pandas.read_csv(io.StringIO(dataLib.runCommand("gam print users query \"orgUnitPath='/KS-Pupils'\" ou")))
+#usernames = pandas.read_csv(io.StringIO(dataLib.runCommand("gam print users query \"orgUnitPath='/KS-Pupils'\" ou")))
+usernames = pandas.read_csv(io.StringIO(dataLib.runCommand("gam print users query ou")))
 for usernamesIndex, usernamesValue in usernames.iterrows():
   usernameMatch = re.match("(.*\..*)(\d\d)@knightsbridgeschool.com", usernamesValue["primaryEmail"])
   if not usernameMatch == None:
     if not usernamesValue["orgUnitPath"] == "/KS-Pupils/" + usernameMatch.group(2):
       print("Moving user " + usernameMatch.group(1) + usernameMatch.group(2) + " to /KS-Pupils/" + usernameMatch.group(2))
-      os.system("gam update user " + usernameMatch.group(1) + usernameMatch.group(2) + " ou /KS-Pupils/" + usernameMatch.group(2))
+      #os.system("gam update user " + usernameMatch.group(1) + usernameMatch.group(2) + " ou /KS-Pupils/" + usernameMatch.group(2))
