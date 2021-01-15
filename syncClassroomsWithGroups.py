0ae880148527cb658a8ae71825aa3bcfc52b1f24
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2021-01-15 14:05:26 +0000

    Update syncClassroomsWithGroups.py

--- syncClassroomsWithGroups.py
+++ syncClassroomsWithGroups.py
@@ -81,6 +81,7 @@ classroomsToSyncIDs = classroomsToSync["ID"].tolist()
 for classroomsIndex, classroomsValue in classrooms.iterrows():
 	if not classroomsValue["id"] in classroomsToSyncIDs:
 		classroomsToSync.append(pandas.DataFrame([classroomsValue["id"], classroomsValue["name"], "", "", ""]))
+classroomsToSync.to_excel(classroomsRoot + os.sep + "classroomsToSync.xlsx", index=false)
 
 sys.exit(0)
 	
